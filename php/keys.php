7af032b4c4e1172ca8ffa8afac6e47ff37193c4b
RobertChristopher <robertchristopherdev11@gmail.com>
2015-07-18 00:07:48 -0400

    Update keys.php

--- php/keys.php
+++ php/keys.php
@@ -2,7 +2,7 @@
 
 // Specify our app keys.
 
-    define("appId","kJawWr0883JUddCTgipaPphexvb3tPUY3Cq9E5uE");
-    define("restKey","ZYiM8CPrDCgvOxhoslMSPT5qN05EhTqqFwOkP2LA");
+    define("appId",<APP KEY>);
+    define("restKey",<REST KEY>);
 
 ?>
